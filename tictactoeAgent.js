0c366a508b9aa0b302e8a7ef2b631bc23ff016db
tarik <tarikm2@uw.edu>
2016-11-11 22:31:39 -0800

    cleaned up and reduced the code. added comments

--- tictactoeAgent.js
+++ tictactoeAgent.js
@@ -1,8 +1,5 @@
 
-var Agent = function () {
-
-
-} //creating an agent object
+var Agent = function () { };     //creating an agent object
 
 //the select method uses minMax to recursively find a solution
 Agent.prototype.selectMove = function (board) {
@@ -17,37 +14,36 @@ function minMax(board, max, min) {
     }
     
     var freeCells = getFreeCells(board);
-    var result = (isPlayerOne(board)) ? { utility: max }
-                                       : { utility: min };
+    var result = board.playerOne ? { utility: max } : { utility: min };
 
-    for (var cell = 0; cell < freeCells.length; cell++) {
-        var copy = board.clone();
-        copy.move(freeCells[cell]);
-        var temp = minMax(copy, max, min);
+    for (var cell = 0; cell < freeCells.length; cell++) {  //for each possible free move
+        var copy = board.clone();       //make a simulated game board and
+        copy.move(freeCells[cell]);     //make the free move
+        var temp = minMax(copy, max, min);  //send this choice down to the next layer in the game
 
-        if (isPlayerOne(board))   //maximize value
+        if (board.playerOne)   //maximize value
         {
-            if (max < temp.utility) {
-                max = temp.utility;
-                result = { utility: temp.utility, move: freeCells[cell] };
+            if (temp.utility > max) {   //if this choice gives us more utility than previous options
+                max = temp.utility; //update our best winning result
+                result = { utility: temp.utility, move: freeCells[cell] }; //set this result to our choice
 
-                if (temp.utility == 1) return result;
+                if (temp.utility == 1) return result;   //if we can win return result
             }       
         }
         else       //minimize utility
         {
-            if (min > temp.utility) {
-                min = temp.utility;
-                result = { utility: temp.utility, move: freeCells[cell] };
+            if (min > temp.utility) {  //if this choice gives us a new minimum utility
+                min = temp.utility;     //update our best min utility
+                result = { utility: temp.utility, move: freeCells[cell] };  //choose this as our best choice
 
-                if (temp.utility == -1) return result;
+                if (temp.utility == -1) return result;  //if we can force their loss, do it
             }
         }
     }
-   return result;  
+   return result;  //return the best choice for this layer in the game
 }
 
-
+//this determines the utility of a given game result
 function utility(board) {
     if (board.gameOver() == 1) {   //x won
         return 1;
@@ -58,20 +54,12 @@ function utility(board) {
     else if (board.gameOver() == 3) { // tie game
         return 0;
     }
-    return "ERROR, UNDEFINED END GAME STATE";
 }
 
 function getFreeCells(board) {
     var freeCells = [];
     for (var cell = 1; cell < 10; cell++) {
         if (board.cellFree(cell)) freeCells.push(cell);
-        
     }
     return freeCells;
-}
-
-// Checks if current player is player 1(maximizer)
-function isPlayerOne(board) {
-    var isP1 = getFreeCells(board).length % 2 == 1
-    return isP1;
 }
